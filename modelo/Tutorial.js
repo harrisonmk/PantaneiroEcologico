184ad0dfa4eeaa19fc36c8f0cae2cb2b97b648d0
Josandonas <34492404+Josandonas@users.noreply.github.com>
2019-11-11 10:08:29 -0300

    adicionando banco de tutoriais e modificando o app.js

---
+++ modelo/Tutorial.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const tutorial = new Schema({
+
+    titulo: {
+        type: String,
+        required: true
+
+
+    },
+
+    subtitulo: {
+        type: String,
+        required: true
+
+
+    },
+
+
+    data: {
+
+        type: Date,
+        default: Date.now()
+
+    },
+
+    texto: {
+        type: String,
+        required: true
+
+
+    }
+    autor: {
+
+        type: String,
+        require:true
+    },
+});
+
+mongoose.model("tutorial", tutorial);
